a829f6463c3470ff1c8be329daeda25028b98a5c
Brian Hoang <bhoang35@gmail.com>
2018-10-24 20:18:11 +0100

    Updates

---
+++ precache-manifest.19ff36379c3b423bcd1fca93da338133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e918834bc3d5234313ed",
+    "url": "/fantasy-sim/static/js/runtime~main.e918834b.js"
+  },
+  {
+    "revision": "2881aa33db378c9c8a1d",
+    "url": "/fantasy-sim/static/js/main.2881aa33.chunk.js"
+  },
+  {
+    "revision": "1c591c195c996fdca2d3",
+    "url": "/fantasy-sim/static/js/1.1c591c19.chunk.js"
+  },
+  {
+    "revision": "2881aa33db378c9c8a1d",
+    "url": "/fantasy-sim/static/css/main.af469332.chunk.css"
+  },
+  {
+    "revision": "e559ccdf3662c844504b0731829c3faa",
+    "url": "/fantasy-sim/index.html"
+  }
+];
